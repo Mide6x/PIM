7140abcf69b2a810d980428cf7a7092f3d3db937
Olumide <adewoleolumide05@gmail.com>
2024-08-06 11:28:15 +0100

    UI Updates

--- client/src/pages/sidebar/Topbar.jsx
+++ client/src/pages/sidebar/Topbar.jsx
@@ -1,6 +1,8 @@
 import useAuth from "../../contexts/useAuth";
 import "./Sidebar.css";
 import userImage from "../../assets/user.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
 
 const Topbar = () => {
   const { userData } = useAuth();
@@ -9,14 +11,17 @@ const Topbar = () => {
     <>
       {userData && (
         <div className="topbarContent">
+          <div className="icon">
+          <FontAwesomeIcon icon={faBell} size="2xl" style={{color: "#1c60ff"}} />
+          </div>
           <div className="userImage">
             <img src={userImage} className="logo-img2" alt="User" />
           </div>
           <div>
-            <h3 style={{ fontSize: "20px" }}>
+            <h3 style={{ fontSize: "18px" }}>
               Hi, <span>{userData.name}</span> <span>.</span>
             </h3>
-            <p style={{ fontSize: "13px", color: "#878787" }}>
+            <p style={{ fontSize: "12px", color: "#878787" }}>
               {userData.email}
             </p>
           </div>
